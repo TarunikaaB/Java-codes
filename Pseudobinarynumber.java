334b1c77e25dc51a40fd274b12a174bdaf365d1e
Tarunikaa B <tarunikatiku23@gmail.com>
2025-05-28 23:25:05 +0530

    Update Pseudobinarynumber.java

--- Pseudobinarynumber.java
+++ Pseudobinarynumber.java
@@ -1,30 +1,30 @@
 import java.util.Scanner;
-public class Pseudobinarynumber
-{
-    public static void main(String[] args)
-    {
+
+public class Pseudobinarynumber {
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.err.println("Enter a number: ");
-        int num = input.nextInt(); ///num = 53
-        int pv,cons,dig;
-        while(num!=0)/// 42!=0
-        {
-            pv=1;
-            cons=0;
-            while (num / pv != 0) /// 0!=0
-            {
-                dig = (num / pv) % 10; ///dig = 5%10=5
-                if (dig > 0)/// 5>0
-                    cons = 1 * pv + cons;//cons=1*10+1=11
-                else
-                    cons = 0 * pv + cons;
-                pv = pv * 10;///pv=100
+        System.out.println("Enter a Number: ");
+        int num = input.nextInt();
+
+        while (num != 0) {
+            int pv = 1, cons = 0, temp = num;
+
+            while (temp != 0) {
+                int dig = temp % 10;
+                if (dig > 0) {
+                    cons += pv;
+                }
+                temp /= 10;
+                pv *= 10;
             }
-            num = num - cons; ///num=53-11=42
-            if(num==0)
-                System.out.println(cons);
-            else
-                System.out.print(cons+"+");///11
+
+            num -= cons;
+
+            if (num == 0) {
+                System.out.print(cons);
+            } else {
+                System.out.print(cons + "+");
+            }
         }
     }
 }
